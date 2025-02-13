51e4da7a5a37b35d7c65f0cf4f165f9d64de551a
risingisland <risingisland@yahoo.com>
2025-02-13 09:53:31 +0000

    Add files via upload

---
+++ alterlang/lang/es_ES.php
@@ -0,0 +1,52 @@
+<?php
+
+$i18n = [
+	
+# Basics
+	'lang_Menu_Title'			=>	'Opciones de AlterLang',
+	
+	'lang_Page_Title'			=>	'AlterLang',
+	'lang_Description'			=>	'Multilingüe real para Getsimple. URL únicas. <br>Mejor SEO. Ayuda a gestionar varias versiones traducidas de una página al permitirle seleccionar páginas equivalentes en diferentes idiomas.',
+	
+# Settings
+	'lang_Base_Lang'			=>	'Lenguaje Base',
+	'lang_Which_Parent'			=>	'¿Qué página principal representa el idioma principal del sitio? (idioma madre)',
+	'lang_Translated_From'		=>	'El idioma desde el que se traducirán las demás páginas.',
+	
+	'lang_Alt_Lang'				=>	'Alternativas',
+	'lang_Add_Lang'				=>	'Agregar plantillas de idioma: (idiomas alternativos: página y local)',
+	'lang_Choose_Page'			=>	'Seleccione la página...',
+	'lang_Choose_Local'			=>	'Local...',
+	'lang_Restore'				=>	'Restaurar anterior',
+	'lang_Configuration'		=>	'configuración',
+	'lang_Old_Configuration'	=>	'¡Configuración anterior restaurada exitosamente!',
+	
+	'lang_Save'					=>	'Guardar',
+	'lang_Delete'				=>	'✖',
+	
+	'lang_About'				=>	'Acerca de',
+	'lang_Purpose'				=>	'Objetivo',
+	'lang_Purpose_Text'			=>	'El objetivo es proporcionar una solución multilingüe real para Getsimple, URL únicas (mejor SEO), sin el uso de i18n, funciona con el CMS básico de Getsimple. Permite agregar páginas alternativas al mapa del sitio. Use el siguiente código en su tema para cambiar entre idiomas.',
+	'lang_Switch_Lang'			=>	'Ejemplo de código para cambiar de idioma',
+	'lang_Flag_Idea'			=>	'Aquí hay una idea para cambiar el idioma con la bandera.',
+	'lang_Flag_Idea_Output'		=>	'Que generará este código',
+	'lang_EN_Meaning'			=>	'"en" proviene de la página madre "en", que significa slug en inglés (identificación de la página del sistema)',
+	'lang_Then_Style'			=>	'Luego se le aplica el siguiente estilo CSS (3 líneas)',
+	'lang_Repeat_CSS'			=>	'Repita este CSS para cada indicador de idioma que necesite para el proyecto multilingüe.',
+	'lang_Pages_Structure'		=>	'Estructura de las páginas',
+	'lang_Pages_Structure_Text'	=>	'Tienes que organizar tus páginas con la madre y los niños.',
+	'lang_First_Lang'			=>	'Primera lengua - página madre',
+	'lang_EN_Alt'				=>	'EN es francés alternativo en este ejemplo',
+	'lang_Page'					=>	'Página',
+	'lang_Child_Page'			=>	'Página infantil',
+	'lang_Sitemap'				=>	'Mapa del sitio',
+	'lang_Better_SEO'			=>	'Mejor SEO: Tienes que instalar <a href="https://github.com/GetSimpleCMS-CE-plugins/plugin-MetaRobots/archive/refs/heads/main.zip">MetaRobots</a> plugin para imprimir páginas alternativas en el mapa del sitio con etiquetas para motores de búsqueda',
+	'lang_And'					=>	'y',
+	
+# Edit Page
+	'lang_Lang_Trans'			=>	'Idiomas y Traducciones',
+	'lang_Alt_Pages'			=>	'Selección de páginas alternativas',
+	'lang_Edit'					=>	'Editar la',
+	'lang_Version'				=>	'Versión',
+	'lang_No_Designated_Page'	=>	'Aún no ha designado ninguna página como categoría de idioma o no tiene páginas secundarias.',
+];
---
+++ alterlang/lang/pl_PL.php
@@ -0,0 +1,52 @@
+<?php
+
+$i18n = [
+	
+# Basics
+	'lang_Menu_Title'			=>	'AlterLang Options',
+	
+	'lang_Page_Title'			=>	'AlterLang',
+	'lang_Description'			=>	'Real Multilingual for Getsimple. Uniques URLs. <br>Better SEO. Helps dealing with several translated version of one page by allowing you to select equivalent pages in different languages.',
+	
+# Settings
+	'lang_Base_Lang'			=>	'Base Language',
+	'lang_Which_Parent'			=>	'Which parent page represents the main language of the site? (mother language)',
+	'lang_Translated_From'		=>	'The language from which the other pages will be translated',
+	
+	'lang_Alt_Lang'				=>	'ALTERNATIVES',
+	'lang_Add_Lang'				=>	'Add language templates: (alternate languages: page and local)',
+	'lang_Choose_Page'			=>	'Choose Page...',
+	'lang_Choose_Local'			=>	'Local...',
+	'lang_Restore'				=>	'Restore previous',
+	'lang_Configuration'		=>	'configuration',
+	'lang_Old_Configuration'	=>	'Old configuration restored successfully!',
+	
+	'lang_Save'					=>	'Save',
+	'lang_Delete'				=>	'✖',
+	
+	'lang_About'				=>	'About',
+	'lang_Purpose'				=>	'PURPOSE',
+	'lang_Purpose_Text'			=>	'The purpose is to provide a real multilingual solution for Getsimple, uniques URLs (Better SEO), without the use of i18n, it is working with basic Getsimple CMS. It allows to add alternative pages to the sitemap. Use the following code in your theme to switch between lang',
+	'lang_Switch_Lang'			=>	'SWITCH LANG CODE EXAMPLE',
+	'lang_Flag_Idea'			=>	'Here is an idea for switching lang with flag',
+	'lang_Flag_Idea_Output'		=>	'Which will output this code',
+	'lang_EN_Meaning'			=>	'"en" comes from the mother page "en" which stands for english slug (system page id)',
+	'lang_Then_Style'			=>	'then it is styled with the following CSS (3 lines)',
+	'lang_Repeat_CSS'			=>	'Repeat this CSS for each lang flag you needed to the multilingual project.',
+	'lang_Pages_Structure'		=>	'PAGES STRUCTURE',
+	'lang_Pages_Structure_Text'	=>	'You have to organize your pages with the mother and children',
+	'lang_First_Lang'			=>	'FIRST LANGUAGE - mother page',
+	'lang_EN_Alt'				=>	'EN is alternate french in this example',
+	'lang_Page'					=>	'Page',
+	'lang_Child_Page'			=>	'child page',
+	'lang_Sitemap'				=>	'SITEMAP',
+	'lang_Better_SEO'			=>	'Better SEO : You have to install <a href="https://github.com/GetSimpleCMS-CE-plugins/plugin-MetaRobots/archive/refs/heads/main.zip">MetaRobots</a> plugin for printing alternate pages in the Sitemap with tags for search engine',
+	'lang_And'					=>	'and',
+	
+# Edit Page
+	'lang_Lang_Trans'			=>	'Languages & translations',
+	'lang_Alt_Pages'			=>	'Selection of alternative pages',
+	'lang_Edit'					=>	'Edit the',
+	'lang_Version'				=>	'Version',
+	'lang_No_Designated_Page'	=>	'You have not yet designated any pages as a language category or they do not have child pages.',
+];
